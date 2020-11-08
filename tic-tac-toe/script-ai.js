55f15824eb93a031b5bd999d6e1955162a1dd107
FrancescoZingarelli <francy@zingarelli.biz>
2020-11-08 19:48:51 +0100

    changed tic-tac-toe emojis

--- tic-tac-toe/script-ai.js
+++ tic-tac-toe/script-ai.js
@@ -55,7 +55,7 @@ function draw(cells) {
       if (cells[x + y * 3]) {
         let text = document.createElement("span")
         text.className = "symbol"
-        text.textContent = cells[x + y * 3] === 'human' ? '🧍' : '🤖'
+        text.textContent = cells[x + y * 3] === 'human' ? '👦' : '🤖'
         cellElement.textContent = ''
         cellElement.appendChild(text)
         cellElement.className = 'cell'
